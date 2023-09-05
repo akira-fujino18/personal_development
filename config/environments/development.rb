318ba6bbe902e5aefaa926a9cc13c21e0c767c7e
fujino-akira <imitation.word@icloud.com>
2023-09-05 20:09:49 +0900

    不具合修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.web_console.allowed_ips = '0.0.0.0/0'
+
+  config.hosts << "vast-beyond-70645-25ef5db239ab.herokuapp.com"
 end
